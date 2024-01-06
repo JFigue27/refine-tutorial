fc5c2f75f29c30be07cfa7b17269246addb6e518
Jose Jaime Figueroa Barrios <jaime.figueroa@inspiracode.net>
2024-01-06 12:10:59 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import { GitHubBanner, Refine, WelcomePage } from '@refinedev/core';
+import { DevtoolsPanel, DevtoolsProvider } from '@refinedev/devtools';
+import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar';
 
-import { notificationProvider, RefineSnackbarProvider } from "@refinedev/mui";
+import {
+  ErrorComponent,
+  notificationProvider,
+  RefineSnackbarProvider,
+} from '@refinedev/mui';
 
-import CssBaseline from "@mui/material/CssBaseline";
-import GlobalStyles from "@mui/material/GlobalStyles";
+import CssBaseline from '@mui/material/CssBaseline';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import routerBindings, {
   DocumentTitleHandler,
+  NavigateToResource,
   UnsavedChangesNotifier,
-} from "@refinedev/react-router-v6";
-import dataProvider from "@refinedev/simple-rest";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ColorModeContextProvider } from "./contexts/color-mode";
+} from '@refinedev/react-router-v6';
+import dataProvider from '@refinedev/simple-rest';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ColorModeContextProvider } from './contexts/color-mode';
+import { MuiInferencer } from '@refinedev/inferencer/mui';
 
 function App() {
   return (
@@ -21,22 +27,41 @@ function App() {
       <RefineKbarProvider>
         <ColorModeContextProvider>
           <CssBaseline />
-          <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+          <GlobalStyles styles={{ html: { WebkitFontSmoothing: 'auto' } }} />
           <RefineSnackbarProvider>
             <DevtoolsProvider>
               <Refine
                 notificationProvider={notificationProvider}
                 routerProvider={routerBindings}
-                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                dataProvider={dataProvider('https://api.fake-rest.refine.dev')}
+                resources={[
+                  {
+                    name: 'blog_posts',
+                    list: '/blog-posts',
+                    show: '/blog-posts/show/:id',
+                    create: '/blog-posts/create',
+                    edit: '/blog-posts/edit/:id',
+                  },
+                ]}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
                   useNewQueryKeys: true,
-                  projectId: "LNsoMj-HhMAQa-6F0aq4",
+                  projectId: 'LNsoMj-HhMAQa-6F0aq4',
                 }}
               >
                 <Routes>
-                  <Route index element={<WelcomePage />} />
+                  <Route
+                    index
+                    element={<NavigateToResource resource="blog_posts" />}
+                  />
+                  <Route path="blog-posts">
+                    <Route index element={<MuiInferencer />} />
+                    <Route path="show/:id" element={<MuiInferencer />} />
+                    <Route path="edit/:id" element={<MuiInferencer />} />
+                    <Route path="create" element={<MuiInferencer />} />
+                  </Route>
+                  <Route path="*" element={<ErrorComponent />} />
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
